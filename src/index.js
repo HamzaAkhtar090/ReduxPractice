6789616013dbb5de5d57211e896351824df75d8c
HamzaAkhtar090 <hamzaasad090@gmail.com>
2020-06-13 01:29:43 +0500

    correction :Provider was imported from wrong module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore , Provider } from 'redux'
+import {createStore  } from 'redux'
 import reducer from './store/reducer'
 import './index.css';
 import App from './App';
