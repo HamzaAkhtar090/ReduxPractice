eacbe99663540cdfb5d953131bdf51c04cc0f016
HamzaAkhtar090 <hamzaasad090@gmail.com>
2020-06-13 01:21:33 +0500

    reducer and provider in redux is introduced in code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore , Provider } from 'redux'
+import reducer from './store/reducer'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+     <Provider>
+       <App />
+     </Provider> 
+      
+  , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+
+initialState = {
+  counter: 0,
+}
+
+const reducer=(state = initialState , action)=>{
+
+
+
+  
+  return state;
+}
+export {reducer};
